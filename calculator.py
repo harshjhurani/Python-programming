cbfac5313d5611e149341b0280b62add89cf9dc5
harshjhurani <jhuraniharsh123@gmail.com>
2024-12-11 22:18:39 +0530

    calculator

---
+++ calculator.py
@@ -0,0 +1,8 @@
+a = int(input('enter a number : '))
+b = int(input('enter a number : '))
+
+
+print('addition = ',(a + b))
+print('substraction = ',(a - b))
+print('multiplication = ',(a * b))
+print('division = ',(a / b))
