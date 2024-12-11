0d464153732f1354969803b9c36de43fb62ff506
harshjhurani <jhuraniharsh123@gmail.com>
2024-12-11 16:25:20 +0530

    hello

---
+++ hello.py
@@ -0,0 +1,13 @@
+print("Hello world")
+
+def print_greetings(name):
+    print("Happy Birthday {}".format(name))
+
+
+
+li = ["Aman", "Deepak", "Harsh","Aman", "Deepak", "Harsh","Aman", "Deepak", "Harsh"]
+
+for el in li:
+    print_greetings(el)
+
+
